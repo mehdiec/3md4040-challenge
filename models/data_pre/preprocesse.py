e36ab6abde6ac08d5534301f0c79401e673d8cb8
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2021-12-20 20:24:34 +0100

    wip

--- models/data_pre/preprocesse.py
+++ models/data_pre/preprocesse.py
@@ -9,10 +9,137 @@ import os.path
 import copy
 from PIL import Image
 from matplotlib import cm
-from const import TRAIN_CONST
-from plankton import PlanktonsDataset
 
+# from plankton import PlanktonsDataset
+from torch.utils.data import Dataset
+import pandas as pd
+
+TRAIN_CONST = [
+    "000_Candaciidae",
+    "043_larvae__Annelida",
+    "001_detritus",
+    "044_Rhopalonema",
+    "002_Calocalanus_pavo",
+    "045_egg__other",
+    "003_larvae__Crustacea",
+    "046_tail__Appendicularia",
+    "004_Podon",
+    "047_Euchirella",
+    "005_Sapphirinidae",
+    "048_calyptopsis",
+    "006_Calanidae",
+    "049_Haloptilus",
+    "007_zoea__Decapoda",
+    "050_eudoxie__Diphyidae",
+    "008_Gammaridea",
+    "051_egg__Actinopterygii",
+    "009_Oikopleuridae",
+    "052_nectophore__Diphyidae",
+    "010_Hyperiidea",
+    "053_head",
+    "011_zoea__Galatheidae",
+    "054_Penilia",
+    "012_nectophore__Physonectae",
+    "055_egg__Cavolinia_inflexa",
+    "013_Rhincalanidae",
+    "056_Pontellidae",
+    "014_Acantharea",
+    "057_Coscinodiscus",
+    "015_Foraminifera",
+    "058_Acartiidae",
+    "016_nauplii__Crustacea",
+    "059_Corycaeidae",
+    "017_gonophore__Diphyidae",
+    "060_artefact",
+    "018_metanauplii",
+    "061_cirrus",
+    "019_megalopa",
+    "062_Luciferidae",
+    "020_Brachyura",
+    "063_Limacinidae",
+    "021_tail__Chaetognatha",
+    "064_cyphonaute",
+    "022_Doliolida",
+    "065_part__Copepoda",
+    "023_Scyphozoa",
+    "066_Fritillariidae",
+    "024_Ctenophora",
+    "067_Echinoidea",
+    "025_Bivalvia__Mollusca",
+    "068_Neoceratium",
+    "026_ephyra",
+    "069_Phaeodaria",
+    "027_Temoridae",
+    "070_Ostracoda",
+    "028_scale",
+    "071_Centropagidae",
+    "029_Evadne",
+    "072_Ophiuroidea",
+    "030_Copilia",
+    "073_nauplii__Cirripedia",
+    "031_Eucalanidae",
+    "074_Salpida",
+    "032_Pyrosomatida",
+    "075_Oithonidae",
+    "033_nectophore__Abylopsis_tetragona",
+    "076_eudoxie__Abylopsis_tetragona",
+    "034_Actinopterygii",
+    "077_cypris",
+    "035_Creseidae",
+    "078_Oncaeidae",
+    "036_Calanoida",
+    "079_gonophore__Abylopsis_tetragona",
+    "037_Decapoda",
+    "080_Harpacticoida",
+    "038_Obelia",
+    "081_Cavoliniidae",
+    "039_Noctiluca",
+    "082_Aglaura",
+    "040_Spumellaria",
+    "083_Euchaetidae",
+    "041_Chaetognatha",
+    "084_Tomopteridae",
+    "042_Annelida",
+    "085_Limacidae",
+]
 DIM = 100, 100
+
+
+class PlanktonsDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+
+        self.data = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.name = self.data.iloc[:, 0]
+        self.targets = self.data.iloc[:, 1]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        landmarks = self.data.iloc[idx, 2:]
+        landmarks = np.array([landmarks])
+        landmarks = landmarks.astype("float").reshape(-1, 28)
+
+        img, target = landmarks, int(self.targets[idx])
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, target
 
 
 def compute_mean_std(loader):
