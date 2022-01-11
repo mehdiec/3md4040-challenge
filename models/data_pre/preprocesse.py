925c545609d14ce8ae579887d52c440dbd69c679
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2022-01-11 22:16:09 +0100

    implemented stratified split

--- models/data_pre/preprocesse.py
+++ models/data_pre/preprocesse.py
@@ -2,11 +2,11 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.transforms import RandomAffine
 
+from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 import cv2
 import os.path
 import copy
-from sklearn.model_selection import StratifiedShuffleSplit
 
 # from plankton import PlanktonsDataset
 from torch.utils.data import Dataset, Subset
@@ -210,24 +210,47 @@ def transform_images_from_folder(train_dir):
 
 # save this stuff somewhere
 # split in validation and training test set
-def make_stratified_split(train_valid_dataset, valid_ratio=0.2):
 
-    df = pd.read_csv("data/train/train_csv/train.csv")
-    target = df["img_name"]
-    x = df["img_class"]
 
-    # 2.
-    split = StratifiedShuffleSplit(n_splits=1, test_size=valid_ratio, random_state=42)
-    for train_index, test_index in split.split(x, target):
-        train_index = train_index
-        test_index = test_index
+def make_stratified_split(train_valid_dataset, valid_ratio=0.2, cache=False):
 
-    print("Train size: {}\nValid size: {}".format(len(train_index), len(test_index)))
+    if cache:
+        with open("data/cache/train.npy", "rb") as f:
+            train_index = np.load(f)
+        with open("data/cache/valid.npy", "rb") as f:
+            test_index = np.load(f)
+        print(train_index, test_index)
+        return [
+            Subset(train_valid_dataset, train_index),
+            Subset(train_valid_dataset, test_index),
+        ]
 
-    return [
-        Subset(train_valid_dataset, train_index),
-        Subset(train_valid_dataset, test_index),
-    ]
+    else:
+        df = pd.read_csv("data/train/train_csv/train.csv")
+        target = df["img_name"]
+        x = df["img_class"]
+
+        # 2.
+        split = StratifiedShuffleSplit(
+            n_splits=1, test_size=valid_ratio, random_state=42
+        )
+        for train_index, test_index in split.split(x, target):
+            train_index = train_index
+            test_index = test_index
+            print(test_index)
+
+        print(
+            "Train size: {}\nValid size: {}".format(len(train_index), len(test_index))
+        )
+
+        with open("data/cache/train.npy", "wb") as f:
+            np.save(f, train_index)
+        with open("data/cache/valid.npy", "wb") as f:
+            np.save(f, test_index)
+        return [
+            Subset(train_valid_dataset, train_index),
+            Subset(train_valid_dataset, test_index),
+        ]
 
 
 def load_coakroaches(
@@ -238,6 +261,7 @@ def load_coakroaches(
     dataset_dir=None,
     train_augment_transforms=None,
     normalizing_tensor_path=None,
+    stratified=False,
 ):
 
     if not dataset_dir:
@@ -252,9 +276,26 @@ def load_coakroaches(
 
     # Split it into training and validation sets
 
-    train_dataset, valid_dataset = make_stratified_split(
-        train_valid_dataset, valid_ratio=0.2
+    print("spliting")
+
+    nb_train, nb_valid = int((1.0 - valid_ratio) * len(train_valid_dataset)), int(
+        valid_ratio * len(train_valid_dataset)
     )
+    if nb_train + nb_valid != len(train_valid_dataset):
+        nb_train = nb_train + 1
+
+    if stratified:
+
+        train_dataset, valid_dataset = make_stratified_split(
+            train_valid_dataset, valid_ratio=0.2, cache=True
+        )
+    else:
+        train_dataset, valid_dataset = torch.utils.data.dataset.random_split(
+            train_valid_dataset, [nb_train, nb_valid]
+        )
+
+    print("DONE spliting")
+
     test_dataset = PlanktonsDataset(
         csv_file="data/test/test_csv/test.csv", root_dir="data/test/", test=True
     )
