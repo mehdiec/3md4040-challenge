ffcafdac0caa6c3b1ddd9afeaf8f969cd35f2f70
Nalpon Mathieu <mathieu.nalpon@student-cs.fr>
2022-02-06 19:44:52 +0000

    comment added

--- src/main.py
+++ src/main.py
@@ -7,12 +7,13 @@ parser.add_argument('work', choices=["train","test"],type=str,help="Type of work
 parser.add_argument('path', type=str)
 parser.add_argument('path_test', type=str, nargs="?")
 args = parser.parse_args()
-#"/mounts/Datasets1/ChallengeDeep/"
+# Exemple for train & test set path for the challenge "/mounts/Datasets1/ChallengeDeep/". Our code automatically checks in train and test folder
+# of the path
 if args.work == "train" : 
-    os.system("python3 train.py --use_gpu --num_workers 8 --normalize --data_augment --model resnet --dataset_dir" + " " + args.path)
+    os.system("python3 src/train.py --use_gpu --num_workers 8 --normalize --data_augment --model resnet --dataset_dir" + " " + args.path)
     #print("python3 train.py --use_gpu --num_workers 8 --normalize --data_augment --model resnet --dataset_dir" + " " + args.path)
 
 
 else:
-    os.system("python3 test.py --use_gpu --num_workers 8 --normalize --model resnet --paramfile "+ args.path +" --dataset_dir "+ args.path_test)
+    os.system("python3 src/test.py --use_gpu --num_workers 8 --normalize --model resnet --paramfile "+ args.path +" --dataset_dir "+ args.path_test)
     #print("python3 test.py --use_gpu --num_workers 8 --normalize --model resnet --paramfile "+ args.path +" --dataset_dir "+ args.path_test)
