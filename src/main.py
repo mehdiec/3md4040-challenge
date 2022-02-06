206012784587ea5dc1de4c4b75fc0812da00e21a
Nalpon Mathieu <mathieu.nalpon@student-cs.fr>
2022-02-06 19:45:13 +0000

    deleting prints

--- src/main.py
+++ src/main.py
@@ -11,9 +11,9 @@ args = parser.parse_args()
 # of the path
 if args.work == "train" : 
     os.system("python3 src/train.py --use_gpu --num_workers 8 --normalize --data_augment --model resnet --dataset_dir" + " " + args.path)
-    #print("python3 train.py --use_gpu --num_workers 8 --normalize --data_augment --model resnet --dataset_dir" + " " + args.path)
+
 
 
 else:
     os.system("python3 src/test.py --use_gpu --num_workers 8 --normalize --model resnet --paramfile "+ args.path +" --dataset_dir "+ args.path_test)
-    #print("python3 test.py --use_gpu --num_workers 8 --normalize --model resnet --paramfile "+ args.path +" --dataset_dir "+ args.path_test)
+
